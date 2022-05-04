2848499ffa6c2c7a8b2f474de738ef0f1f3475e8
Gary Chen <chenhhgary@gmail.com>
2022-05-04 21:54:19 +0800

    add a few sample queries

---
+++ sql_script/queries/customer/customer_add.sql
@@ -0,0 +1,2 @@
+insert into `customer` (`name`, `gender`, `age`, `address`, `contact`, `balance`, `password`) values
+('Honghao CHEN', 'male', '21', 'Shaw College, CUHKSZ', '3170@link.cuhk.edn.cn', '1200', 'abc123');
---
+++ sql_script/queries/customer/customer_update.sql
@@ -0,0 +1,3 @@
+UPDATE `customer`
+SET `gender` = 'female', `age` = '19', `address` = 'new address'
+WHERE `customer_id` = 1;
